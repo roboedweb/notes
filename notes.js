ecbf685e9c79f0d5ffbac4a935e313499a95bc99
roboedweb <119788832+roboedweb@users.noreply.github.com>
2023-05-21 14:36:06 +0300

    Add files via upload

--- notes.js
+++ notes.js
@@ -4,12 +4,20 @@ const filterButton = document.getElementById("filter-button");
 const notesList = document.getElementById("notes-list");
 const formWrapper = document.querySelector(".form-wrapper");
 const formExpand = document.querySelector(".form-expand");
+const findString = document.getElementById("find");
+const findButton = document.getElementById("find-button");
+const findExpand = document.querySelector(".find-expand");
+const findWrapper = document.querySelector(".find-wrapper");
 
 formExpand.addEventListener('click', () => {
     if (formExpand.classList.contains("form-disabled")) {
         formExpand.classList.remove("form-disabled");
         formExpand.classList.add("form-enabled");
         formWrapper.classList.add("form-active");
+        findExpand.classList.add("find-disabled");
+        findExpand.classList.remove("find-enabled");
+        findWrapper.classList.remove("find-active");
+        findExpand.innerHTML = '&#1422';
         formExpand.innerHTML = '&#8593';
     }
     else {
@@ -20,12 +28,32 @@ formExpand.addEventListener('click', () => {
     }
 });
 
+findExpand.addEventListener('click', () => {
+    if (findExpand.classList.contains("find-disabled")) {
+        findExpand.classList.remove("find-disabled");
+        findExpand.classList.add("find-enabled");
+        findWrapper.classList.add("find-active");
+        findExpand.innerHTML = '&#8595';
+        formExpand.classList.add("form-disabled");
+        formExpand.classList.remove("form-enabled");
+        formWrapper.classList.remove("form-active");
+        formExpand.innerHTML = '+';
+    }
+    else {
+        findExpand.classList.add("find-disabled");
+        findExpand.classList.remove("find-enabled");
+        findWrapper.classList.remove("find-active");
+        findExpand.innerHTML = '&#1422';
+    }
+});
+
 document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
     addNote();
 });
 
 filterButton.addEventListener("click", filterNotes);
+findButton.addEventListener("click", findNotes);
 noteInput.addEventListener('input', autoresize);
 
 let notes = JSON.parse(localStorage.getItem("notes")) || [];
@@ -65,4 +93,13 @@ function filterNotes() {
 function autoresize() {
     this.style.height = 'auto';
     this.style.height = this.scrollHeight - 20 + 'px';
+}
+
+function findNotes() {
+    const findedNotes = [];
+    notes.forEach((note) => {
+        note.text.includes(findString.value) ? findedNotes.push(note) : false;
+    });
+    findString.value = '';
+    findedNotes.length > 0 ? renderNotes(findedNotes) : renderNotes([{text: 'not found', date: 'try another word'}]);
 }
